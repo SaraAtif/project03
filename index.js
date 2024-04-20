4ca68a9d427475ee04cd41764b562a212de6f8fb
Sara <saraatif135@gmail.com>
2024-04-21 01:31:52 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//Question 7:
+console.log(5 + 3);
+console.log(11 - 2);
+console.log(4 * 2);
+console.log(32 / 4);
+//Question 8;
+//add
+var num = 5;
+var num1 = 3;
+var sum = num + num1;
+console.log(sum);
+//sub
+var num2 = 11;
+var sub = num2 - num1;
+console.log(sub);
+//Multiply
+var num3 = 4;
+var num4 = 2;
+var mul = num3 * num4;
+console.log(mul);
+//Division
+var num5 = 32;
+var divide = num5 / num3;
+console.log(divide);
+//Question 9
+var favnum = 5;
+console.log("My favourite number is: ".concat(favnum));
---
+++ index.ts
@@ -0,0 +1,33 @@
+//Question 7:
+console.log(5 + 3);
+console.log(11 - 2);
+console.log(4 * 2);
+console.log(32 / 4);
+
+
+//Question 8;
+//add
+let num = 5;
+let num1 = 3;
+let sum = num + num1;
+console.log(sum);
+
+//sub
+let num2 = 11;
+let sub = num2 - num1;
+console.log(sub);
+
+//Multiply
+let num3 = 4;
+let num4 = 2;
+let mul = num3 * num4;
+console.log(mul);
+
+//Division
+let num5 = 32;
+let divide = num5 / num3;
+console.log(divide);
+
+//Question 9
+let favnum:number=5;
+console.log(`My favourite number is: ${favnum}`);
